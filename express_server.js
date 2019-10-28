559d71137a2cc480ae10460bfd0df3e63748136d
Ted Ogmundson <togmund@gmail.com>
2019-10-28 22:01:59 +0000

    added url routing to handle /urls.json object

--- express_server.js
+++ express_server.js
@@ -1,16 +1,27 @@
+// Requirements and dependencies
 const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
 
+
+// Ephemeral Data to push into the page
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": "http://www.meduim.com",
+  "9sm5xK": "https://old.reddit.com/r/nba/"
 };
 
+
+// URL routing
 app.get("/", (req, res) => {
   res.send("Hello, there!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+
+// Listener
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
