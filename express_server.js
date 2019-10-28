353e05fe584d66de73b119f975757a3109d41917
Ted Ogmundson <togmund@gmail.com>
2019-10-28 22:11:36 +0000

    added /hello route with some welcome text in HTML format

--- express_server.js
+++ express_server.js
@@ -6,8 +6,9 @@ const PORT = 3000; // default port 8080
 
 // Ephemeral Data to push into the page
 const urlDatabase = {
-  "b2xVn2": "http://www.meduim.com",
-  "9sm5xK": "https://old.reddit.com/r/nba/"
+  "b2xVn2": "http://www.medium.com",
+  "9sm5xK": "https://old.reddit.com/r/nba/",
+  "hoopsh": "https://streamable.com/0lru8"
 };
 
 
@@ -20,8 +21,14 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 
 // Listener
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
