c92f30f624f1b2fb6aedbb25ea5fea7fbecc43e6
Ted Ogmundson <togmund@gmail.com>
2019-10-29 00:04:09 +0000

    reorganized folders, added partial and _header.ejs file. Debugging issues related to, I think, filepath stuff

--- express_server.js
+++ express_server.js
@@ -23,12 +23,12 @@ app.get("/", (req, res) => {
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render("/urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+  res.render("/urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
